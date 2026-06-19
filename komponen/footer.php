a7f97b586ce71f771ef6655a3feb9f05f9083af6
yassss <ayyas.husayn@gmail.com>
2026-06-19 16:19:14 +0700

    footer

--- komponen/footer.php
+++ komponen/footer.php
@@ -1,15 +1,15 @@
 <footer class="footer">
     <div class="footer-content animate-on-scroll">
         <div class="footer-section about">
-            <h3>CabaiDomba Farm</h3>
+            <h3>Tumbutani Nusantara</h3>
             <p>Menghasilkan cabai domba berkualitas tinggi dengan metode pertanian modern dan berkelanjutan.</p>
         </div>
         <div class="footer-section links">
             <h3>Hubungi Kami</h3>
             <ul>
-                <li><a href="https://wa.me/6281234567890" target="_blank">WhatsApp Kami</a></li>
-                <li><a href="https://instagram.com/cabaidombafarm" target="_blank">Instagram</a></li>
-                <li><a href="https://facebook.com/cabaidombafarm" target="_blank">Facebook</a></li>
+                <li><a href="https://api.whatsapp.com/send/?phone=6282260067197&text=Halo+Tumbutani+Nusantara&type=phone_number&app_absent=0" target="_blank">WhatsApp Kami</a></li>
+                <li><a href="https://instagram.com/tumbutani.nusantara" target="_blank">Instagram</a></li>
+                <li><a href="https://facebook.com/TUMBUTANI NUSANTARA" target="_blank">Facebook</a></li>
             </ul>
         </div>
         <div class="footer-section maps">
